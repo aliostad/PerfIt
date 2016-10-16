cd3d3ddb81db88f44f7ff874860d6b47401486db
Michael W Powell <mwpowellhtx@gmail.com>
2016-10-16 17:04:08 -0400

    Added a PublishInstrumentationDelegate to the model. This helps to document what will supersede the anonymous Action along the same lines.

---
+++ src/PerfIt/PublishInstrumentationDelegate.cs
@@ -0,0 +1,12 @@
+﻿namespace PerfIt
+{
+    /// <summary>
+    /// PublishInstrumentationDelegate callback definition.
+    /// </summary>
+    /// <param name="categoryName"></param>
+    /// <param name="instanceName"></param>
+    /// <param name="elapsedMilliseconds"></param>
+    /// <param name="instrumentationContext"></param>
+    public delegate void PublishInstrumentationDelegate(string categoryName, string instanceName,
+        double elapsedMilliseconds, string instrumentationContext);
+}
