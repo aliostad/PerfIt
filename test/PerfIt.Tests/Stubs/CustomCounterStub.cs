6db9676edaabc82b02ec7512a907f25db9310332
Ivan Marx <ivan.marx@asos.com>
2017-01-11 13:31:02 +0000

    Only call handlers for counters specified in SimpleInstrumentor constructor

---
+++ test/PerfIt.Tests/Stubs/CustomCounterStub.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using PerfIt.Handlers;
+
+namespace PerfIt.Tests.Stubs
+{
+    public class CustomCounterStub : CounterHandlerBase
+    {
+        public static int RequestStartCount { get; private set; }
+        public static int RequestEndCount { get; private set; }
+
+        public static void ClearCounters()
+        {
+            RequestStartCount = 0;
+            RequestEndCount = 0;
+        }
+
+        public CustomCounterStub(string categoryName, string instanceName) : base(categoryName, instanceName)
+        {            
+        }
+
+        public override string CounterType { get; }
+        protected override void OnRequestStarting(IDictionary<string, object> contextBag, PerfItContext context)
+        {
+            RequestStartCount++;
+        }
+
+        protected override void OnRequestEnding(IDictionary<string, object> contextBag, PerfItContext context)
+        {
+            RequestEndCount++;
+        }
+
+        protected override void BuildCounters(bool newInstanceName = false)
+        {            
+        }
+
+        protected override CounterCreationData[] DoGetCreationData()
+        {
+            return new CounterCreationData[] {};
+        }
+    }
+}
