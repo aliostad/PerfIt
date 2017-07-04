c5e6b77e9202d7d69a2ffea0877655c0a2ec1c61
Ali Kheyrollahi <aliostad@gmail.com>
2017-07-04 08:30:21 +0100

    Adding tracer

---
+++ src/PerfIt/ITwoStageTracer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfIt
+{
+    public interface ITwoStageTracer
+    {
+        object Start(IInstrumentationInfo info);
+
+        void Finish(object token, string instrumentationContext = null);
+    }
+}
