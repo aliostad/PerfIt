1c0041951b053c12e50d79e17f534933b10684a6
Ali Kheyrollahi <aliostad@gmail.com>
2018-03-05 22:35:37 +0000

    with config malarky

--- src/PerfIt/PerfItRuntime.cs
+++ src/PerfIt/PerfItRuntime.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 
+#if NETSTANDARD2_0
+using Microsoft.Extensions.Configuration;
+#endif
+
 namespace PerfIt
 {
     public static class PerfItRuntime
     {
+
+#if NETSTANDARD2_0
+        static PerfItRuntime()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection().Build();
+        }
+
+        public static IConfiguration Configuration { get; set; }
+#endif
         public static string GetUniqueName(string instanceName, string counterType)
         {
             return string.Format("{0}.{1}", instanceName, counterType);
@@ -53,13 +68,21 @@ namespace PerfIt
 
         private static string GetConfigurationValue(string key, string delimiter, string categoryName = null)
         {
-            throw new NotImplementedException();
-            //if (categoryName == null)
-            //    return ConfigurationManager.AppSettings[string.Format("{0}{1}{2}", 
-            //        Constants.PerfItConfigurationPrefix, delimiter, key)];
-            //else
-            //    return ConfigurationManager.AppSettings[string.Format("{0}{1}{2}{1}{3}", 
-            //        Constants.PerfItConfigurationPrefix, delimiter, key, categoryName)];
+            if (categoryName == null)
+                return ReadConfig(string.Format("{0}{1}{2}",
+                    Constants.PerfItConfigurationPrefix, delimiter, key));
+            else
+                return ReadConfig(string.Format("{0}{1}{2}{1}{3}",
+                    Constants.PerfItConfigurationPrefix, delimiter, key, categoryName));
+        }
+
+        private static string ReadConfig(string key)
+        {
+#if NET452
+            return System.Configuration.ConfigurationManager.AppSettings[key];
+#else
+            return Configuration[key];
+#endif
         }
 
         public static double GetSamplingRate(string categoryName, double defaultValue)
